4b04fa6bd2852d8b71ba1f246eede27094ad8b2a
jieeee-1 <jie2341029@gmail.com>
2024-11-19 11:26:39 +0800

    1119

--- mr-react-todo/todo-demo/src/components/Todo.jsx
+++ mr-react-todo/todo-demo/src/components/Todo.jsx
@@ -1,11 +1,23 @@
+import { MdDelete, MdEdit } from "react-icons/md";
 
 // 解構props的屬性名稱，用{屬性名}包起來
-function Todo({ todo }) {
+function Todo({ todo, deleteTodo, toggleCompleted }) {
     return (
-        <div className="todo">
+        // 新增一個completed類別規則
+        // 使用反引號+三元運算子檢查isCompleted
+        // 如果為真=>套用completed類別規則
+        // 如果為否=>取消completed類別規則  =>''
+        <div className={`todo ${todo.isCompleted ? 'completed' : ''}`}>
             {/* <p>上課中</p> */}
             {/* <p>{todo}</p> */}
-            <p>{todo.content}</p>
+            <p onClick={() => { toggleCompleted(todo.id) }}>{todo.content}</p>
+            <div>
+                <MdEdit style={{ cursor: "pointer" }} />
+                <MdDelete
+                    onClick={() => { deleteTodo(todo.id) }}
+                    style={{ cursor: "pointer", marginLeft: "5px" }}
+                />
+            </div>
         </div>
     )
 }
--- mr-react-todo/todo-demo/src/components/TodoWrapper.jsx
+++ mr-react-todo/todo-demo/src/components/TodoWrapper.jsx
@@ -7,16 +7,41 @@ function TodoWrapper() {
     // 因為有n個todo，所以使用陣列存取
     // const [todos, setTodos] = useState(['list1', 'list2']);
     // 因為陣列需要key屬性，所以要加上id
+    // const [todos, setTodos] = useState([
+    //     { content: 'list1', id: Math.random() },
+    //     { content: 'list2', id: Math.random() }
+    // ]);
+
+    // 因為要判定todo是否被點擊，所以要增加一個標記屬性=>isCompleted
     const [todos, setTodos] = useState([
-        { content: 'list1', id: Math.random() },
-        { content: 'list2', id: Math.random() }
+        { content: 'list1', id: Math.random(), isCompleted: false },
+        { content: 'list2', id: Math.random(), isCompleted: false },
     ]);
 
     // 建立加入新的todo內容
     // 1. 使用...其餘運算子來保留原陣列內容
     // 2. 再加入新的物件內容
     const addTodo = (content) => {
-        setTodos([...todos, { content: content, id: Math.random }])
+        setTodos([...todos, { content: content, id: Math.random(), isCompleted: false }])
+    }
+
+    // 建立刪除todo函式
+    // 使用filter方法，去除被刪除的todo
+    const deleteTodo = (id) => {
+        setTodos(todos.filter((todo) => {
+            // 檢查目前的id是否為被刪除的id
+            // 如果不是，則保留
+            return todo.id !== id
+        }))
+    }
+
+    // 建立雙向(toggle)切「完成與取消」的函式
+    const toggleCompleted = (id) => {
+        setTodos(todos.map((todo) => {
+            return todo.id === id
+                ? { ...todo, isCompleted: !todo.isCompleted }
+                : todo
+        }))
     }
 
     return (
@@ -25,7 +50,10 @@ function TodoWrapper() {
             <CreateForm addTodo={addTodo} />
             {
                 todos.map((todo) => {
-                    return <Todo todo={todo} key={todo.id} />
+                    return <Todo todo={todo} key={todo.id}
+                        deleteTodo={deleteTodo}
+                        toggleCompleted={toggleCompleted}
+                    />
                 })
             }
         </div>
