1e49f8f12d7a32e9ba8436cc5cb712c5457796ae
jieeee-1 <jie2341029@gmail.com>
2024-12-24 12:00:56 +0800

    1224

--- my-react-app1/src/App.jsx
+++ my-react-app1/src/App.jsx
@@ -1,47 +1,46 @@
-import { useEffect } from "react";
+import { motion } from "motion/react"
 
-import $ from 'jquery';
+function App() {
+  // 摺疊資料
+  const questions = [
+    {
+      id: 1,
+      question: "題目1",
+      answer: "答案1",
+    },
+    {
+      id: 2,
+      question: "題目2",
+      answer: "答案2",
+    },
+    {
+      id: 3,
+      question: "題目3",
+      answer: "答案3",
+    },
+  ];
 
-export default function App() {
-
-  // 建議的寫法2
-  useEffect(() => {
-    // 移入 => mouseover()
-    // 移出 => mouseout()
-    // 移動 => mousemove()
-    // X、Y座標 => pageX、pageY
-    // 取得HTML內容 => html()
-    // 淡入 => fadeIn()
-    // 淡出 => fadeOut()
-    // 速度 => 毫秒 => (slow, normal, fast)
-
-    $('a:has(.ttpShow)').mouseover(function (e) {
-      $('body').append('<div id="ttpPanel">' + $().children(".ttpShow").html() + '</div>');
-      $('#ttpPanel').css({
-        top: (e.pageY) + 'px',
-        left: (e.pageX) + 'px'
-      }).fadeIn("fast");
-    }).on('mouseout', function () {
-      $('#ttpPanel').remove();
-    }).on('mousemove', function (e) {
-      $('#ttpPanel').css({
-        top: (e.pageY + 10) + 'px',
-        left: (e.pageX + 10) + 'px'
-      });
-    })
-  }, [])
-
-  return (
+  return(
     <>
-      <h2>ToolTip</h2>
-      <p>Lorem, ipsum dolor sit amet
-        <a href="#">
-          consectetur
-          <span className="ttpShow">consectetur 內容說明...</span>
-        </a>
-        in minus esse, quas eos quaerat nisi deserunt autem? Labore tempore adipisci, minus esse excepturi recusandae!
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Praesentium non facilis officiis. Sunt, dolore.
-        Lorem, ipsum dolor.i deserunt autem? Labore temporei deserunt autem? Labore temporei deserunt autem? Labore tempore</p>
-    </>
-  )
+    <div className="wrapper" style={{
+      backgroundColor: "black",
+      maxWidth:"100vh",
+      height:"100vh",
+      display:"flex",
+      justifyContent:"center",
+      paddingTop:"50px",
+      color:"#ccc",
+    }}>
+      {/* FAQ區 */}
+      <div className="faq" style={{
+        backgroundColor:"gray",
+        width:"80%",
+      }}>
+        {/* 主標題 */}
+        <h2>FAQ列表</h2>
+      </div>
+      </div></>
+  );
 }
+
+export default App;
